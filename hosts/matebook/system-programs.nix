63ce0ba0f1ec4feb3a99baa328651d798ecc5df8
Simon Babovic <simon@babovic.at>
2025-03-13 13:56:40 +0100

    add mathematica install comment

--- hosts/matebook/system-programs.nix
+++ hosts/matebook/system-programs.nix
@@ -2,8 +2,7 @@
   environment.systemPackages = with pkgs; [
    (mathematica.override {
     source = pkgs.requireFile {
-      # nix-store --query --hash $(nix store add-path Mathematica_13.3.1_LINUX.sh --name 'Mathematica_13.3.1_LINUX.sh')
-      # maybe use nix-store --add-root <some_path_here_for_symlink> --indirect --realise /nix/store/...
+      # nix-store --add-root <symlink> --indirect --realise $(nix store add-path Mathematica_13.3.1_LINUX.sh --name 'Mathematica_13.3.1_LINUX.sh')
       name = "Mathematica_13.3.1_LINUX.sh";
       sha256 = "1xl6ji8qg6bfz4z72b8czl0cx36fzfkxhygsn0m8xd0qgkkpjqfg";
       message = ''
--- hosts/workstation/system-programs.nix
+++ hosts/workstation/system-programs.nix
@@ -2,7 +2,7 @@
   environment.systemPackages = with pkgs; [
     (mathematica.override {
     source = pkgs.requireFile {
-      # nix-store --query --hash $(nix store add-path Mathematica_12.3.1_LINUX.sh --name 'Mathematica_12.3.1_LINUX.sh')
+      # nix-store --add-root <symlink> --indirect --realise $(nix store add-path Mathematica_13.3.1_LINUX.sh --name 'Mathematica_12.3.1_LINUX.sh')
       name = "Mathematica_12.3.1_LINUX.sh";
       sha256 = "1yfd287kv46a1qr53kdnshl0qb868hyl44mh1r1kl4k90x6kdqnc";
       message = ''
