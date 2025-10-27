b125170aa9f8b16e22b39be437eb05fd9de0630c
Simon Babovic <simon@babovic.at>
2025-10-27 12:39:48 +0100

    add openscad

--- modules/desktop/system-programs.nix
+++ modules/desktop/system-programs.nix
@@ -34,6 +34,8 @@
     jdk
     ipmiview
     rstudio
+    openscad
+    # bambu-studio
   ];
 
   environment.variables = {
@@ -71,13 +73,13 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
+      # ovmf = {
+      #   enable = true;
+      #   packages = [(pkgs.OVMF.override {
+      #     secureBoot = true;
+      #     tpmSupport = true;
+      #   }).fd];
+      # };
     };
   };
 
