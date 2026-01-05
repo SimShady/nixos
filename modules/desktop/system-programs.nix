398ec32635c867f31b1beb538518cbdf30a2da82
Simon Babovic <simon@babovic.at>
2026-01-05 12:52:45 +0100

    add possibility for shared folders between host and vm

--- modules/desktop/system-programs.nix
+++ modules/desktop/system-programs.nix
@@ -72,13 +72,7 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      # ovmf = {
-      #   enable = true;
-      #   packages = [(pkgs.OVMF.override {
-      #     secureBoot = true;
-      #     tpmSupport = true;
-      #   }).fd];
-      # };
+      vhostUserPackages = with pkgs; [ virtiofsd ];
     };
   };
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
